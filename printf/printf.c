aed48a52406f5192ee73bac08e1254db8d5630bc
Eliel <elielmichel@gmail.com>
2022-03-23 18:15:10 -0300

    Adicionar arquivos de projeto.

---
+++ printf/printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PRECO 1.99
+
+int main() {
+	int pera = 3;
+	char qualidade = 'A';
+	float peso = 0.653;
+
+	printf("Existem %d peras de qualidade %c ", pera, qualidade);
+	printf("pesando %f quilos.\n", peso);
+	printf("O preco por quilo e %f, total e %f\n", PRECO, peso * PRECO);
+
+	return 0;
+}
